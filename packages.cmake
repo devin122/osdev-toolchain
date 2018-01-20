b14835b8984f34c071b454e4ef256448b9e147c5
Devin Nakamura <devin122@gmail.com>
2018-01-20 18:50:04 -0500

    Refactor versions into new file

---
+++ packages.cmake
@@ -0,0 +1,14 @@
+# Versions / URLS for the various packages that are downloaded
+
+set(TOOLCHAIN_BINUTILS_VERSION 2.27)
+set(TOOLCHAIN_BINUTILS_HASH 41b053ed4fb2c6a8173ef421460fbb28)
+set(TOOLCHAIN_BINUTILS_URL ftp://ftp.gnu.org/gnu/binutils/binutils-${TOOLCHAIN_BINUTILS_VERSION}.tar.gz)
+
+
+set(GCC_VERSION 6.3.0)
+set(GCC_HASH MD5=6e5ea04789678f1250c1b30c4d9ec417)
+set(GCC_URL ftp://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz)
+
+set(NASM_VERSION 2.12.02)
+set(NASM_HASH MD5=908b09944aa2251244565c00458b535a)
+set(NASM_URL http://www.nasm.us/pub/nasm/releasebuilds/${NASM_VERSION}/nasm-${NASM_VERSION}.tar.gz)
