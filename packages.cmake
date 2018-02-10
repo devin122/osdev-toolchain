903b6726de960ef7e0c88e8cd01d06e3b1e664f6
Devin Nakamura <devin122@gmail.com>
2018-02-09 19:03:37 -0500

    Update toolchain versions

--- packages.cmake
+++ packages.cmake
@@ -1,8 +1,20 @@
 # Versions / URLS for the various packages that are downloaded
 
-set(BINUTILS_VERSION 2.27)
-set(BINUTILS_HASH 41b053ed4fb2c6a8173ef421460fbb28)
+set(BINUTILS_VERSION 2.29.1)
+set(BINUTILS_HASH MD5=522B762FD362532D14726BF476F55366)
 set(BINUTILS_URL ftp://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.gz)
+
+set(MPC_VERSION 1.1.0)
+set(MPC_URL "https://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz")
+set(MPC_HASH MD5=4125404E41E482EC68282A2E687F6C73)
+
+set(MPFR_VERSION 4.0.0)
+set(MPFR_URL "http://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.gz")
+set(MPFR_HASH MD5=574A8713F1417913ED8C75FE75F56CFC)
+
+set(GMP_VERSION 6.1.2)
+set(GMP_URL "https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.xz")
+set(GMP_HASH MD5=F58FA8001D60C4C77595FBBB62B63C1D)
 
 set(GCC_VERSION 6.3.0)
 set(GCC_HASH MD5=6e5ea04789678f1250c1b30c4d9ec417)
@@ -11,3 +23,7 @@ set(GCC_URL ftp://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.
 set(NASM_VERSION 2.12.02)
 set(NASM_HASH MD5=908b09944aa2251244565c00458b535a)
 set(NASM_URL http://www.nasm.us/pub/nasm/releasebuilds/${NASM_VERSION}/nasm-${NASM_VERSION}.tar.gz)
+
+set(ISL_VERSION 0.18)
+set(ISL_HASH MD5=076C69F81067F2F5B908C099F445A338)
+set(ISL_URL http://isl.gforge.inria.fr/isl-${ISL_VERSION}.tar.gz)
